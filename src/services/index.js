d8cf895b02ecaf381d5e618c034532d20e0e953f
soumyaranjansahoo338 <soumyaranjansahoo338@gmail.com>
2021-05-10 23:05:14 +0530

    "Drive upload added."

--- src/services/index.js
+++ src/services/index.js
@@ -2,6 +2,7 @@ import user from './user/user.service';
 import account from './account/account.service';
 import upload from './upload/upload.service';
 import chequeBookRequest from './cheque-book-request/cheque-book-request.service';
+import uploadTest from './upload-test/upload-test.service';
 
 // eslint-disable-next-line no-unused-vars
 export default function (app) {
@@ -9,4 +10,5 @@ export default function (app) {
     app.configure(account);
     app.configure(upload);
     app.configure(chequeBookRequest);
+    app.configure(uploadTest);
 }
